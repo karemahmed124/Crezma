ac5217f6781559733e9adaec5f3c687f55082679
karemahmed124 <42273386+karemahmed124@users.noreply.github.com>
2019-05-25 20:38:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,3 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "-";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Victoria ,!`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
 
 client.on("message", message => {
     var prefix = "-";
